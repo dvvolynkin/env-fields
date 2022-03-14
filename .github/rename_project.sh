382c9df8980a1094422187d9a646125cb0794592
dvvolynkin <dvvolynkin@users.noreply.github.com>
2022-03-14 10:55:18 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="dvvolynkin"
+original_name="env_fields"
+original_urlname="env-fields"
+original_description="Awesome env_fields created by dvvolynkin"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv env_fields $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,6 @@
 import pytest
 
-from project_name import BaseClass, base_function
+from env_fields import BaseClass, base_function
 
 given = pytest.mark.parametrize
 
