8633efd060eba633e3d80e317883da1a681721e6
Daniil Volynkin <dvvolynkin@avito.ru>
2022-03-14 18:59:46 +0300

    Init lib

--- env_fields/__init__.py
+++ env_fields/__init__.py
@@ -1,3 +1,49 @@
-from .base import BaseClass, base_function
+import os
+from functools import partial
+from dataclasses import field
 
-__all__ = ["BaseClass", "base_function"]
+_EmptyType = type('_EmptyType', (object,), {})
+NO_DEFAULT_VALUE = _EmptyType()
+
+
+class EnvVarParsingError(Exception):
+    ...
+
+
+def get_env_value(cast, name, default=NO_DEFAULT_VALUE):
+    value = os.getenv(name)
+    if value is None:
+        if default == NO_DEFAULT_VALUE:
+            raise EnvVarParsingError(
+                (
+                    f"Environment variable {name} not set."
+                    " Default value for this case not set too."
+                )
+            )
+
+        return default
+
+    return cast(value)
+
+
+def env_field(name, cast, default=NO_DEFAULT_VALUE, **kwargs):
+    return field(
+        default_factory=partial(
+            get_env_value,
+            name=name,
+            cast=cast,
+            default=default,
+        ),
+        **kwargs,
+    )
+
+
+def _specific_env_field(cast):
+    return partial(
+        env_field,
+        cast=cast
+    )
+
+
+env_field_str = _specific_env_field(str)
+env_field_int = _specific_env_field(int)
---
+++ tests/test_all.py
@@ -0,0 +1,38 @@
+import os
+from dataclasses import dataclass
+from typing import Any
+
+import pytest
+
+from env_fields import(
+    env_field_int,
+    env_field_str,
+    env_field,
+)
+
+given = pytest.mark.parametrize
+
+
+@given("env_field_fn, env_var_value, expected_res_value", [
+    (env_field_int, "12", 12),
+    (env_field_str, "12", "12"),
+])
+def test_ready_field_types(monkeypatch, env_field_fn, env_var_value, expected_res_value):
+    monkeypatch.setattr(os, 'environ', {'SOME_ENV_VAR_NAME': env_var_value})
+
+    @dataclass
+    class SomeDataClass:
+        env_var: Any = env_field_fn('SOME_ENV_VAR_NAME')
+
+    assert SomeDataClass().env_var == expected_res_value
+
+
+def test_env_field(monkeypatch):
+    @dataclass
+    class SomeDataClass:
+        env_var: Any = env_field('SOME_ENV_VAR_NAME', str, default=12323)
+
+    assert SomeDataClass().env_var == 12323
+    monkeypatch.setattr(os, 'environ', {'SOME_ENV_VAR_NAME': "hello, world"})
+    assert SomeDataClass().env_var == "hello, world"
+
